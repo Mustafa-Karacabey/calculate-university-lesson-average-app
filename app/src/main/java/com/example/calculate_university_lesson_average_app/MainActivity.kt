9f748cb89577f3771c96db05dcc731893944688b
Mustafa KARACABEY <mustafa.karacabey.engineer@gmail.com>
2021-10-11 22:58:14 +0300

    [Updated-layout]

--- app/src/main/java/com/example/calculate_university_lesson_average_app/MainActivity.kt
+++ app/src/main/java/com/example/calculate_university_lesson_average_app/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.calculate_university_lesson_average_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun calculateAverage(v : View) {
+
     }
 }
